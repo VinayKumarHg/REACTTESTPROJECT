7f8932a015e116c3ff68244894b7e7b59c4a645d
VinayKumarHg <vinaykumarmercury1997@gmail.com>
2021-06-09 12:09:09 +0530

    inti

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/Header"
+import './App.css';
+import Navbar from "./components/Navbar"
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Navbar/>
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+
+function Header(){
+    return (
+
+<h1 className="Header"> Welcome Portfolio</h1>
+    );
+}
+export default Header;
